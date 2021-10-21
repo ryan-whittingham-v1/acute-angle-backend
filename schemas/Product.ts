30aef42764ac13e75a2fa65d6ab50284acd6d2a2
Ryan Whittingham <whittingham777@gmail.com>
2021-10-20 22:35:36 -0700

    update access

--- schemas/Product.ts
+++ schemas/Product.ts
@@ -4,15 +4,15 @@ import { rules, isSignedIn, permissions } from '../access';
 
 export const Product = list({
   access: {
-    create: permissions.canManageProducts,
+    /*   create: permissions.canManageProducts,
     read: true,
     update: permissions.canManageProducts,
-    delete: permissions.canManageProducts,
+    delete: permissions.canManageProducts, */
   },
   ui: {
-    hideCreate: (args) => !permissions.canManageProducts(args),
+    /* hideCreate: (args) => !permissions.canManageProducts(args),
     hideDelete: (args) => !permissions.canManageProducts(args),
-    isHidden: (args) => !permissions.canManageProducts(args),
+    isHidden: (args) => !permissions.canManageProducts(args), */
   },
   fields: {
     name: text({ isRequired: true }),
